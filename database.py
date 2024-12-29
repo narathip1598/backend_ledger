665a9d8f1cd9d7ce80a0f9a0b135b792036b771e
narathip1598 <narathip@blendata.co>
2024-12-29 09:49:10 +0700

    first commit

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:024280575z@localhost:5432/QuizApplication"
+DATABASE_URL = "postgresql://narathip1598:9ikwIhTrHhO7gfZZCt6qL8t9BBCOOHnA@dpg-ctobfjpopnds73fguf2g-a/quizapplication"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
